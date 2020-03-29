2a48d1542e667598b22f901fce1e6ccd9e968513
Santhosh <santhoshdm@Santhoshs-MacBook-Air.local>
2020-03-29 16:24:42 +0530

    changed app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning Docker!
+          Docker isnt working yet!
         </p>
         <a
           className="App-link"
